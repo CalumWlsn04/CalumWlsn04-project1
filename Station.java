305c24c7662dc5f8df554b541d611e8cff0df62a
Calum Wilson <calum8285@gmail.com>
2024-10-25 15:59:35 -0400

    I believe that I have finished this, need to clean up and supply comments on my recursive method

--- Station.java
+++ Station.java
@@ -71,11 +71,13 @@ public class Station
     {
         int totalMovement = 0;
         TransferStation temp = new TransferStation("blue/pink", "Transfer");
+        EndStation endy = new EndStation("temp", "temp");
         Station temp2;
 
         temp2 = (Station) temp;
         System.out.println(temp2.getClass());
         
+        System.out.println("THis is the dest name: " + dest.name + ", and the dest color: " + dest.lineColor);
         System.out.println("This is the current station: " + current.name + ", and this is the current station type: " + current.getClass() + ", and this is the current linecolor: " + current.lineColor);
         System.out.println("These are the two conditions: " + current.getClass().equals(temp.getClass()) + ", " + !current.lineColor.equals(dest.lineColor));
 
@@ -85,21 +87,37 @@ public class Station
         {
             temp = (TransferStation) current;
             System.out.println("This is the size of the temp otherstations arraylist: " + temp.otherStations.size());
+            int checker = totalMovement;
             for (int i = 0; i < temp.otherStations.size(); i++)
             {
+                System.out.println("This is dest color: " + dest.lineColor);
+                System.out.println("This the station color: " + temp.otherStations.get(i).lineColor);
                 System.out.println("Station Name: " + temp.otherStations.get(i).name + ", True? " + temp.otherStations.get(i).lineColor.equals(dest.lineColor));
-                System.out.println("WHY!!!")
+                System.out.println("WHY!!!" + temp.otherStations.get(i).equals(dest));
+
                 if(temp.otherStations.get(i).equals(dest))
                 {
                     return totalMovement + 1;
                 }
-
-                if(temp.otherStations.get(i).lineColor.equals(dest.lineColor) && !temp.otherStations.get(i).next.name.equals(current.name))
+                else if(temp.otherStations.get(i).lineColor.equals(dest.lineColor))
                 {
-                    System.out.println("HEY");
-                        totalMovement = totalMovement + 1 + tripLengthRecursive(temp.otherStations.get(i), dest);
-                        i = temp.otherStations.size() - 1;
+                    System.out.println("In here!");
+                    if (!temp.otherStations.get(i).getClass().equals(endy.getClass()))
+                    {
+                        System.out.println("Made it here!!!");
+                        System.out.println("This is temp.otherstations.get(i).next.name is " + temp.otherStations.get(i).next.name);
+                        if (!temp.otherStations.get(i).next.name.equals(current.name))
+                        {
+                            System.out.println("Th");
+                            totalMovement = totalMovement + 1 + tripLengthRecursive(temp.otherStations.get(i), dest);
+                            i = temp.otherStations.size() - 1;
+                        }
+                    }
                 }
+            }
+            if (checker == totalMovement)
+            {
+                totalMovement = totalMovement + 1 + tripLengthRecursive(current.next, dest);
             }
         }
         else
